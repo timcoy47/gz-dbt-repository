08d6812368953b70af42042be65a1d20373bcce4
Tim Coy <timcoy72@gmail.com>
2024-04-17 12:06:47 +0000

    reup

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,13 +3,13 @@ WITH sales_data AS (
     SELECT 
         s.date_date,
         s.orders_id,
-        s.pdt_id AS product_id,
+        product_id,
         s.revenue,
         s.quantity,
-        p.purchSE_PRICE
+        p.purchase_price
     FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} AS s
     LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS p
-    ON s.pdt_id = p.products_id
+    ON s.product_id = p.products_id
 )
 SELECT
     date_date,
@@ -17,7 +17,7 @@ SELECT
     product_id,
     revenue,
     quantity,
-    purchSE_PRICE,
-    CAST(quantity AS INT64) * CAST(purchSE_PRICE AS NUMERIC) AS purchase_cost,
-    revenue - (CAST(quantity AS INT64) * CAST(purchSE_PRICE AS NUMERIC)) AS margin
+    purchase_price,
+    CAST(quantity AS INT64) * CAST(purchase_price AS NUMERIC) AS purchase_cost,
+    revenue - (CAST(quantity AS INT64) * CAST(purchase_price AS NUMERIC)) AS margin
 FROM sales_data
